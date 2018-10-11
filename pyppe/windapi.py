78b535968465ccf25c716e829fb488a02edb8b9c
Simon Li <43945943+s-t-li@users.noreply.github.com>
2018-10-12 01:03:44 +0800

    UPDATE 0.1.002
    1. Rewritten function [wind_series].
    2. Added some code comments.

--- pyppe/windapi.py
+++ pyppe/windapi.py
@@ -1,18 +1,33 @@
 import numpy as np
 import pandas as pd
 from WindPy import *
+w.start()
 
 from .tdays import tdays_prev, to_date
 
-def wind_series(wcodes, fields, s_date, e_date, **kwargs):
-    s_date = tdays_prev(to_date(s_date), **kwargs)
-    e_date = tdays_prev(to_date(e_date), **kwargs)
+
+# fetch time series data via Wind API
+def wind_series(wcodes, fields, sdate, edate, **kwargs):
+    """
+    #Func: fetch time series data via Wind API
+    
+    #Params:
+    wcodes: Wind code, e.g: 000300.SH, 000011.OF
+    fields: data field, e.g: "close", "nav", "nav_adj"
+    sdate: start date, e.g: "2018-10-10", datetime.date(2018, 10, 10)
+    edate: end date, e.g: "2018-10-15", datetime.date(2018, 10, 15)
+    
+    #Return:
+    time series data in pandas DataFrame with date index and Wind code columns name
+    """
+    
+    sdate = tdays_prev(to_date(sdate), **kwargs) # most recent trading day of start date
+    edate = tdays_prev(to_date(edate), **kwargs) # most recent trading day of end date
     options = ";".join([k + "=" + v for k, v in kwargs.items()])
     
-    if s_date == e_date:
-        series = quote_value(wcodes, fields, s_date, **kwargs)
-    else:
-        wind_data = w.wsd(wcodes, fields, s_date, e_date, options)
+    if sdate < edate:
+        wind_data = w.wsd(wcodes, fields, sdate, edate, options)
         series = pd.DataFrame(wind_data.Data, index=wind_data.Codes, columns=wind_data.Times).T
-        
-    return series
+        return series
+    else:
+        return None
