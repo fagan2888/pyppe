84b46cd57543391571d5ac8a99ce8c6efccad821
Simon Li <43945943+s-t-li@users.noreply.github.com>
2018-10-14 16:19:23 +0800

    UPDATE 0.1.003
    1. Rewritten function [wind_series] to support multi-codes with multi-indicators.
    2. Refined code comments.

--- pyppe/windapi.py
+++ pyppe/windapi.py
@@ -1,33 +1,58 @@
-import numpy as np
 import pandas as pd
 from WindPy import *
-w.start()
 
 from .tdays import tdays_prev, to_date
 
+w.start()
 
 # fetch time series data via Wind API
 def wind_series(wcodes, fields, sdate, edate, **kwargs):
     """
-    #Func: fetch time series data via Wind API
+    #Func:
+        fetch time series data via Wind API
+        multi-codes with multi-indicators is supported
     
     #Params:
-    wcodes: Wind code, e.g: 000300.SH, 000011.OF
-    fields: data field, e.g: "close", "nav", "nav_adj"
-    sdate: start date, e.g: "2018-10-10", datetime.date(2018, 10, 10)
-    edate: end date, e.g: "2018-10-15", datetime.date(2018, 10, 15)
+        wcodes: Wind code, e.g: "000300.SH", "000011.of", ["000300.SH", "000985.CSI"]
+        fields: data field, e.g: "close", "NAV", "nav_adj", ["high", "low"]
+        sdate: start date, e.g: "20180710", "2018/07/01", "2018-07-10", datetime.date(2018, 07, 10)
+        edate: end date, e.g: "20181015", "2018/10/15", "2018-10-15", datetime.date(2018, 10, 15)
+        **kwargs: ref: https://www.windquant.com/
     
     #Return:
-    time series data in pandas DataFrame with date index and Wind code columns name
+        time series data in pandas DataFrame
+        with date index and Wind code + fields columns (hierarchical columns)
     """
     
     sdate = tdays_prev(to_date(sdate), **kwargs) # most recent trading day of start date
     edate = tdays_prev(to_date(edate), **kwargs) # most recent trading day of end date
-    options = ";".join([k + "=" + v for k, v in kwargs.items()])
-    
-    if sdate < edate:
-        wind_data = w.wsd(wcodes, fields, sdate, edate, options)
-        series = pd.DataFrame(wind_data.Data, index=wind_data.Codes, columns=wind_data.Times).T
-        return series
+
+    # formatting parameters
+    if isinstance(wcodes, str):
+        # wcodes is a list of one Wind code like ["000300.SH"]
+        wcodes = [wcodes]
+
+    if isinstance(fields, list):
+        # fields is a list like ["high", "low"]
+        fields_len = len(fields)
     else:
-        return None
+        # fields is a string like "high"
+        fields_len = 1
+
+    df = pd.DataFrame()
+    
+    for code in wcodes:
+        wind_data = w.wsd(code, fields, sdate, edate, **kwargs) # raw wind data
+        
+        if wind_data.ErrorCode != 0:
+            # Error Code: ref: https://www.windquant.com/
+            raise Exception("Wind Error Code:" + str(wind_data.ErrorCode))
+        
+        wcodes_col = [code] * fields_len # like ["000300.SH", "000300.SH"]
+
+        idx = pd.MultiIndex.from_arrays([wcodes_col, wind_data.Fields])
+        sub_df = pd.DataFrame(wind_data.Data, index=idx, columns=wind_data.Times).T
+        
+        df = pd.concat([df, sub_df], axis=1)
+
+    return df
